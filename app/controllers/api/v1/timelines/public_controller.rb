77572b3d9267de5e337ac56d7d4943015c31890a
Will Schulz <51292950+willschulz@users.noreply.github.com>
2023-04-07 17:08:09 -0400

    went back to jk loop

--- app/controllers/api/v1/timelines/public_controller.rb
+++ app/controllers/api/v1/timelines/public_controller.rb
@@ -8,34 +8,34 @@ class Api::V1::Timelines::PublicController < Api::BaseController
     @statuses = load_statuses
     
     ###
-    weighted_scores = calculate_weighted_scores(@statuses)
-    @statuses = @statuses.sort_by { |status| [-weighted_scores[status.id], -status.created_at.to_i] }
+    #weighted_scores = calculate_weighted_scores(@statuses)
+    #@statuses = @statuses.sort_by { |status| [-weighted_scores[status.id], -status.created_at.to_i] }
     #insert_pagination_headers #this is suggested but doesn't make sense to me
     ###
-    #faves = Array.new(@statuses.length)
-    #for ii in 0...@statuses.length
-    #  faves[ii] = REST::StatusSerializer.new(@statuses[ii]).favourites_count
-    #end
-    #sorted_statuses = @statuses.sort_by.with_index { |_, i| -faves[i] } 
-    #@statuses = sorted_statuses
+    faves = Array.new(@statuses.length)
+    for ii in 0...@statuses.length
+      faves[ii] = REST::StatusSerializer.new(@statuses[ii]).favourites_count
+    end
+    sorted_statuses = @statuses.sort_by.with_index { |_, i| -faves[i] } 
+    @statuses = sorted_statuses
     render json: @statuses, each_serializer: REST::StatusSerializer, relationships: StatusRelationshipsPresenter.new(@statuses, current_user&.account_id)
   end
 
   private
   
   ###
-  def calculate_weighted_scores(statuses)
-    # Calculate the weighted scores for each status
-    statuses.map do |status|
-      likes_count = REST::StatusSerializer.new(status).favourites_count# || 0
-      #retweets_count = status.retweets_count || 0
-      #replies_count = status.replies_count || 0
-      likes_weight = 0.5
-      retweets_weight = 0.3
-      replies_weight = 0.2
-      weighted_score = likes_count * likes_weight #+ retweets_count * retweets_weight + replies_count * replies_weight
-      [status.id, weighted_score]
-  end.to_h
+  #def calculate_weighted_scores(statuses)
+  #  # Calculate the weighted scores for each status
+  #  statuses.map do |status|
+  #    likes_count = REST::StatusSerializer.new(status).favourites_count# || 0
+  #    #retweets_count = status.retweets_count || 0
+  #    #replies_count = status.replies_count || 0
+  #    likes_weight = 0.5
+  #    retweets_weight = 0.3
+  #    replies_weight = 0.2
+  #    weighted_score = likes_count * likes_weight #+ retweets_count * retweets_weight + replies_count * replies_weight
+  #    [status.id, weighted_score]
+  #end.to_h
   ###
 
   def require_auth?
