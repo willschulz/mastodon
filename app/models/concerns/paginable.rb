f3b3dbab9b39ec9a9bb91c1a19a40237ebc70244
willschulz <wm.s.schulz@gmail.com>
2023-04-08 18:16:40 -0400

    but back desc

--- app/models/concerns/paginable.rb
+++ app/models/concerns/paginable.rb
@@ -29,7 +29,7 @@ module Paginable
       # query = query.where(arel_table[:id].lt(max_id)) if max_id.present?
       # query = query.where(arel_table[:id].gt(since_id)) if since_id.present?
       # query
-      query = joins(:status_stat).order(StatusStat.arel_table[:favourites_count].desc, arel_table[:id]).limit(3)#this is where we go back #remember to add back .desc after [:id]
+      query = joins(:status_stat).order(StatusStat.arel_table[:favourites_count].desc, arel_table[:id].desc).limit(3)#this is where we go back #remember to add back  after [:id]
       query = query.where(arel_table[:id].lt(max_id)) if max_id.present?
       query = query.where(arel_table[:id].gt(since_id)) if since_id.present?
       query
