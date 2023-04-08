25ef212a0ccf3d4d1a079c75e17a90bfa92eedd6
willschulz <wm.s.schulz@gmail.com>
2023-04-08 18:41:17 -0400

    only filter fav at end

--- app/models/concerns/paginable.rb
+++ app/models/concerns/paginable.rb
@@ -33,7 +33,7 @@ module Paginable
       query = joins(:status_stat).order(StatusStat.arel_table[:favourites_count].desc, arel_table[:id].desc).limit(3)
       query = query.where(arel_table[:id].lt(max_id)) if max_id.present?
       query = query.where(arel_table[:id].gt(since_id)) if since_id.present?
-      query = query.order(StatusStat.arel_table[:favourites_count].desc, arel_table[:id].desc)
+      query = query.order(StatusStat.arel_table[:favourites_count].desc)
       query
     }
 
