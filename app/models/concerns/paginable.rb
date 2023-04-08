d76e3404369a4cc4bd688b2be7a7af8e6674168f
willschulz <wm.s.schulz@gmail.com>
2023-04-08 17:28:21 -0400

    go back

--- app/models/concerns/paginable.rb
+++ app/models/concerns/paginable.rb
@@ -25,9 +25,7 @@ module Paginable
 
     #my attempt:
     scope :paginate_by_max_id_fav, ->(limit, max_id = nil, since_id = nil) {
-      #query = order(arel_table[:id].desc).limit(limit)
-      query = order(arel_table[:favourites_count].desc).limit(limit)
-      query = query.order(arel_table[:id].desc)
+      query = order(arel_table[:id].desc).limit(limit)
       query = query.where(arel_table[:id].lt(max_id)) if max_id.present?
       query = query.where(arel_table[:id].gt(since_id)) if since_id.present?
       query
