bc3ecb1635b524bc934950d9f70ec6db1bc572f7
willschulz <wm.s.schulz@gmail.com>
2023-04-08 17:51:37 -0400

    try joining to status_stats in pagination

--- app/models/concerns/paginable.rb
+++ app/models/concerns/paginable.rb
@@ -25,7 +25,11 @@ module Paginable
 
     #my attempt:
     scope :paginate_by_max_id_fav, ->(limit, max_id = nil, since_id = nil) {
-      query = order(arel_table[:id].desc).limit(limit)
+      # query = order(arel_table[:id].desc).limit(limit)
+      # query = query.where(arel_table[:id].lt(max_id)) if max_id.present?
+      # query = query.where(arel_table[:id].gt(since_id)) if since_id.present?
+      # query
+      query = joins(:status_stats).order(StatusStat.arel_table[:favourites_count].desc, arel_table[:id].desc).limit(limit)
       query = query.where(arel_table[:id].lt(max_id)) if max_id.present?
       query = query.where(arel_table[:id].gt(since_id)) if since_id.present?
       query
