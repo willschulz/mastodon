0dc7bd8c1269ce62b1039e041a57e3c352f9c189
Will Schulz <51292950+willschulz@users.noreply.github.com>
2023-08-17 16:57:27 -0400

    raise api rate limit

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -62,7 +62,7 @@ class Rack::Attack
     IpBlock.blocked?(req.remote_ip)
   end
 
-  throttle('throttle_authenticated_api', limit: 300, period: 5.minutes) do |req|
+  throttle('throttle_authenticated_api', limit: 99999, period: 5.minutes) do |req|
     req.authenticated_user_id if req.api_request?
   end
 
