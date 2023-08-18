4affe915e6aa18aa4b4454234978ba77255b516e
Will Schulz <51292950+willschulz@users.noreply.github.com>
2023-08-18 15:40:50 -0400

    star to heart

--- app/javascript/mastodon/features/favourited_statuses/index.js
+++ app/javascript/mastodon/features/favourited_statuses/index.js
@@ -76,7 +76,7 @@ class Favourites extends ImmutablePureComponent {
     return (
       <Column bindToDocument={!multiColumn} ref={this.setRef} label={intl.formatMessage(messages.heading)}>
         <ColumnHeader
-          icon='star'
+          icon='heart'
           title={intl.formatMessage(messages.heading)}
           onPin={this.handlePin}
           onMove={this.handleMove}
