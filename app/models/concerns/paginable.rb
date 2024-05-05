8349b8d3633b29f1bfb7ea37f659844f0309e558
Will Schulz <51292950+willschulz@users.noreply.github.com>
2024-05-05 17:25:52 -0400

    Update paginable.rb
    add old modifications to mix likes with bitshifted age. bitshifting needs work but this still should reorder timelines to boost high-liked posts

--- app/models/concerns/paginable.rb
+++ app/models/concerns/paginable.rb
@@ -21,11 +21,37 @@ module Paginable
       query
     }
 
-    def self.to_a_paginated_by_id(limit, options = {})
+    #my attempt:
+    scope :paginate_by_max_id_fav, ->(limit, max_id = nil, since_id = nil) {
+      query = joins(:status_stat)
+              .reorder((Arel::Nodes::Multiplication.new(StatusStat.arel_table[:favourites_count], 100000000000) + Arel::Nodes::BitwiseShiftRight.new(arel_table[:id],22)).desc) #good but bitshift not working as intended
+              .limit(20)
+      query = query.where(arel_table[:id].lt(max_id)) if max_id.present?
+      query = query.where(arel_table[:id].gt(since_id)) if since_id.present?
+      query = query
+      query
+    }
+
+    scope :paginate_by_min_id_fav, ->(limit, min_id = nil, max_id = nil) {
+      query = joins(:status_stat).order(StatusStat.arel_table[:favourites_count], arel_table[:id]).limit(2)
+      query = query.where(arel_table[:id].gt(min_id)) if min_id.present?
+      query = query.where(arel_table[:id].lt(max_id)) if max_id.present?
+      query
+    }
+
+    def self.to_a_paginated_by_id(limit, options = {})#still need this? how to paginate this way for rchron and my new way by timeline interface?
       if options[:min_id].present?
         paginate_by_min_id(limit, options[:min_id], options[:max_id]).reverse
       else
         paginate_by_max_id(limit, options[:max_id], options[:since_id]).to_a
+      end
+    end
+    
+    def self.to_a_paginated_by_id_fav(limit, options = {})
+      if options[:min_id].present?
+        paginate_by_min_id_fav(limit, options[:min_id], options[:max_id]).reverse
+      else
+        paginate_by_max_id_fav(limit, options[:max_id], options[:since_id]).to_a
       end
     end
   end
