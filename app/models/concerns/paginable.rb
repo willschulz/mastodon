7a65fe76e05b9d1f0b8614c43a3212c5793a89e7
Will Schulz <51292950+willschulz@users.noreply.github.com>
2024-05-08 14:45:24 -0400

    add Arel.sql('NUMERIC')

--- app/models/concerns/paginable.rb
+++ app/models/concerns/paginable.rb
@@ -80,7 +80,7 @@ module Paginable
 
       # todo: try casting with Arel (no SqlLiteral)
       numeric_age_in_seconds = Arel::Nodes::NamedFunction.new('CAST', [
-        Arel::Nodes::As.new(age_in_seconds, 'numeric')
+        Arel::Nodes::As.new(age_in_seconds, Arel.sql('NUMERIC'))
       ])
 
       # Calculate the weighted score
