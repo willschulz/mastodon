855aa458ed1449adfab95ec5a2e78c044363eb7f
Will Schulz <wm.s.schulz@gmail.com>
2024-05-20 13:56:09 -0400

    Update index.js
    different hardcoding test

--- app/javascript/mastodon/features/home_timeline/index.js
+++ app/javascript/mastodon/features/home_timeline/index.js
@@ -166,7 +166,8 @@ class HomeTimeline extends React.PureComponent {
         ) : <NotSignedInIndicator />}
 
         <Helmet>
-          <title>{intl.formatMessage(messages.title)}</title>
+          <!--<title>{intl.formatMessage(messages.title)}</title>-->
+          <title>{intl.formatMessage("Hardcoded")}</title>
           <meta name='robots' content='noindex' />
         </Helmet>
       </Column>
