adbf05e90c0ff452d5b3e2afe1d560f031a35372
root <wschulz@stanford.edu>
2024-12-03 09:43:27 +0000

    patches

--- app/models/algo_status_scores.rb
+++ app/models/algo_status_scores.rb
@@ -3,13 +3,14 @@
 # Table name: algo_status_scores
 #
 #  id         :bigint(8)        not null, primary key
-# positive_sentiment_rev_chrono :integer 
+#  positive_sentiment_rev_chrono :integer
+#  negative_sentiment_rev_chrono :integer
 
 class AlgoStatusScores < ExtRecord
     self.table_name = 'algo_status_scores'
     
     def self.get_positive_sentiment_rev_chrono(limit)
-        AlgoStatusScores.where('positive_sentiment_rev_chrono > 0').order(positive_sentiment_rev_chrono: :desc).limit(limit)
+        AlgoStatusScores.where('positive_sentiment_rev_chron > 0').order(positive_sentiment_rev_chrono: :desc).limit(limit)
     end
     
 end
