258d1444450e7888bce59a727a838738c6cc1582
Max Rodriguez <maxlrodwork@gmail.com>
2025-07-24 15:06:11 -0700

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,8 @@ class HomeController < ApplicationController
   include WebAppControllerConcern
 
   before_action :set_instance_presenter
+  # In mastodon/app/controllers/home_controller.rb
+  before_action :require_user!, if: :public_timeline_route?
 
   def index
     expires_in 0, public: true unless user_signed_in?
@@ -13,5 +15,10 @@ class HomeController < ApplicationController
 
   def set_instance_presenter
     @instance_presenter = InstancePresenter.new
+  end
+
+  def public_timeline_route?
+    # Require authentication for public timeline routes
+    request.path.start_with?('/public') && !Setting.timeline_preview
   end
 end
